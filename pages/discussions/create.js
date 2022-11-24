19accf4ed218a77aa47adcebf7852f517c5649bf
Hermes <hermeschigoziri@gmail.com>
2022-11-25 00:26:49 +0100

    add an interface for creating discussions

--- pages/discussions/create.js
+++ pages/discussions/create.js
@@ -1,35 +1,113 @@
 import { Alert, Button } from "@mui/material";
 import Head from "next/head";
-import { Fragment, useContext } from "react";
+import { useRouter } from "next/router";
+import { Fragment, useContext, useEffect, useState } from "react";
 import Input from "../../components/Input/Input";
 import TextArea from "../../components/Input/TextArea";
 import Select from "../../components/Select/Select";
 import { UserAuthContext } from "../../context/UserAuthContext";
-import styles from "../../styles/create-discussion.module.css";
 import { DISCUSSION_TAGS } from "../../utilities/app-utilities";
+import styles from "../../styles/create-discussion.module.css";
+import { supabase } from "../../supabase/config";
 
 export default function Create() {
 	const { profileID } = useContext(UserAuthContext);
+	const [errorText, setErrorText] = useState("");
+	const [createBtnDisabled, setCreateBtnDisabled] = useState(false);
+	const [discussionData, setDiscussionData] = useState({
+		title: "",
+		body: "",
+		tag: DISCUSSION_TAGS[0].toLowerCase(),
+	});
+	const router = useRouter();
 
-	function formSubmitHandler(event) {
-		event.preventDefault();
+	useEffect(() => {
+		const cachedTitle = window.localStorage.getItem("discussion_title");
+		const cachedBody = window.localStorage.getItem("discussion_body");
+		setDiscussionData({
+			title: cachedTitle || "",
+			body: cachedBody || "",
+			tag: DISCUSSION_TAGS[0],
+		});
+	}, []);
+
+	useEffect(() => {
 		if (profileID === null) {
-			throw new Error("No user signed in!");
+			setCreateBtnDisabled(true);
+			setErrorText("You need to be signed in to create a discussion!");
+		} else {
+			setCreateBtnDisabled(false);
+			setErrorText("");
+		}
+	}, [profileID]);
+
+	function onDiscussionCreated() {
+		window.localStorage.removeItem("discussion_title");
+		window.localStorage.removeItem("discussion_body");
+		router.push("/discussions");
+	}
+
+	async function formSubmitHandler(event) {
+		event.preventDefault();
+
+		if (profileID !== null) {
+			setCreateBtnDisabled(true);
+			const data = discussionData;
+			data.creator_id = profileID;
+			try {
+				await supabase.from("discussions").insert(data).throwOnError();
+				onDiscussionCreated();
+			} catch (error) {
+				setErrorText(
+					`Failed to create discussion - ${
+						error.message || error.error_description
+					}`
+				);
+				setCreateBtnDisabled(false);
+			}
 		}
 	}
 
-	const noUser = profileID === null;
+	function onCancelBtnClicked() {
+		const { title, body } = discussionData;
+		const titleTrimmed = title.trim();
+		const bodyTrimmed = body.trim();
+
+		if (titleTrimmed.length > 0) {
+			window.localStorage.setItem("discussion_title", titleTrimmed);
+		}
+		if (bodyTrimmed.length > 0) {
+			window.localStorage.setItem("discussion_body", bodyTrimmed);
+		}
+
+		router.push("/discussions");
+	}
+
+	function onTitleInputValueChanged(event) {
+		setDiscussionData((snapshot) => {
+			return { ...snapshot, title: event.target.value };
+		});
+	}
+
+	function onBodyInputValueChanged(event) {
+		setDiscussionData((snapshot) => {
+			return { ...snapshot, body: event.target.value };
+		});
+	}
+
+	function onSelectChanged(event) {
+		setDiscussionData((snapshot) => {
+			return { ...snapshot, tag: event.target.value };
+		});
+	}
+
 	return (
 		<Fragment>
 			<Head>
 				<title>Animehaven | Create a Discussion</title>
 			</Head>
 			<div className={styles["create-discussion-container"]}>
-				{noUser && (
-					<Alert severity="warning">
-						You need to be signed in to create a discussion!
-					</Alert>
-				)}
+				{errorText.length > 0 && <Alert severity="warning">{errorText}</Alert>}
 				<h2 className="fs-3 mt-3">Create a Discussion</h2>
 				<form onSubmit={formSubmitHandler} className="d-flex flex-column gap-2">
 					<div className="d-flex flex-column">
@@ -40,7 +118,9 @@ export default function Create() {
 							className={styles["title-field"]}
 							spellCheck="false"
 							id="title-field"
-							minLength={5}
+							minLength={4}
+							value={discussionData.title}
+							onChange={onTitleInputValueChanged}
 							required
 						/>
 					</div>
@@ -48,14 +128,21 @@ export default function Create() {
 						<label htmlFor="body-field" className={styles.label}>
 							Body
 						</label>
-						<TextArea minRows={6} id="body-field" required />
+						<TextArea
+							minRows={6}
+							id="body-field"
+							value={discussionData.body}
+							onChange={onBodyInputValueChanged}
+							required
+						/>
 					</div>
 					<div className="d-flex flex-column gap-1">
 						<span className={styles.label}>Tags</span>
-						<Select>
-							{DISCUSSION_TAGS.map((tag) => {
+						<Select value={discussionData.tag} onChange={onSelectChanged}>
+							{DISCUSSION_TAGS.map((tag, index) => {
 								return (
 									<option
+										key={index}
 										disabled={tag === "Announcement"}
 										value={tag.toLowerCase()}>
 										{tag}
@@ -69,10 +156,10 @@ export default function Create() {
 							variant="contained"
 							className={styles["create-btn"]}
 							type="submit"
-							disabled={noUser}>
+							disabled={createBtnDisabled}>
 							Create
 						</Button>
-						<Button type="button" color="error">
+						<Button type="button" color="error" onClick={onCancelBtnClicked}>
 							Cancel
 						</Button>
 					</div>
