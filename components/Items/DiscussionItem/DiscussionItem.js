9f87f3737ff025a2fc818c332e62095a83e4e4c7
Hermes <hermeschigoziri@gmail.com>
2022-11-25 12:06:23 +0100

    show discussions

--- components/Items/DiscussionItem/DiscussionItem.js
+++ components/Items/DiscussionItem/DiscussionItem.js
@@ -1,37 +1,49 @@
-import { Skeleton } from "@mui/material";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import styles from "./style.module.css";
+import { useEffect, useState } from "react";
+import { getProfileData } from "../../../utilities/app-utilities";
+import { Skeleton } from "@mui/material";
 
-export default function DiscussionItem({id, title, author, dataLoaded }) {
-    author = author.toLowerCase();
+export default function DiscussionItem(props) {
+	const [creatorAcctName, setCreatorAcctName] = useState("");
 
-    return (
-      <li className={styles["discussion-item"]}>
-        {dataLoaded ? (
-          <>
-            <Link href={`/discussions/${id}`} className={styles.title}>{title}</Link>
-            <small className={styles.author}>
-              Opened by{" "}
-              <Link href={`/users/${author}`}>
-                {author}
-              </Link>
-            </small>
-          </>
-        ) : (
-          <>
-            <Skeleton variant={"text"} sx={{ fontSize: '1rem', backgroundColor: '#333333' }} width={500} />
-            <Skeleton variant={"text"} sx={{ fontSize: '0.8rem', backgroundColor: '#333333' }} width={100} />
-          </>
-        )
-        }
-      </li>
-    );
+	useEffect(() => {
+		const { creatorID } = props;
+		getProfileData("account_name", creatorID).then((data) => {
+			setCreatorAcctName(data.account_name);
+		});
+	}, [props]);
+
+	const loading = creatorAcctName === "";
+	if (loading) {
+		return (
+			<li className={styles["discussion-item"]}>
+				<Skeleton variant="text" />
+				<div className="d-flex gap-2">
+					<Skeleton variant="text" />
+					<Skeleton variant="text" />
+				</div>
+			</li>
+		);
+	}
+	return (
+		<li className={styles["discussion-item"]}>
+			<div className={styles.tag}>{`#${props.tag}`}</div>
+			<Link href={`/discussions/${props.id}`} className={styles.title}>
+				{props.title}
+			</Link>
+			<small className={styles.creator}>
+				Created by{" "}
+				<Link href={`/users/${creatorAcctName}`}>{creatorAcctName}</Link>
+			</small>
+		</li>
+	);
 }
 
 DiscussionItem.propTypes = {
-    id: PropTypes.string,
-    dataLoaded: PropTypes.bool,
-    title: PropTypes.string,
-    author: PropTypes.string
-}
+	id: PropTypes.number,
+	title: PropTypes.string,
+	creatorID: PropTypes.string,
+	tag: PropTypes.string,
+};
--- pages/discussions/index.js
+++ pages/discussions/index.js
@@ -8,6 +8,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { Fragment, useContext, useMemo, useState } from "react";
 import Button from "../../components/Button/Button";
+import { useEffect } from "react";
 import Checkbox from "../../components/Checkbox/Checkbox";
 import CheckboxList from "../../components/CheckboxList/CheckboxList";
 import SearchInput from "../../components/Input/SearchInput/SearchInput";
@@ -15,24 +16,82 @@ import PageContainer from "../../components/PageContainer/PageContainer";
 import Select from "../../components/Select/Select";
 import { UserAuthContext } from "../../context/UserAuthContext";
 import { DISCUSSION_TAGS } from "../../utilities/app-utilities";
+import { getDiscussionsByTags } from "../../utilities/app-utilities";
+import DiscussionItem from "../../components/Items/DiscussionItem/DiscussionItem";
+import Loading from "../../components/Loading/Loading";
 
 export default function Discussions() {
 	const { profileID } = useContext(UserAuthContext);
+	const [discussionsList, setDiscussionsList] = useState([]);
 	const [filterDrawerIsOpen, setFilterDrawerIsOpen] = useState(false);
+	const [filter, setFilter] = useState("all");
+	const [discussionTags, setDiscussionTags] = useState(() => {
+		const value = {};
+		DISCUSSION_TAGS.forEach((tag) => (value[tag] = true));
+		return value;
+	});
+	const [queryingDB, setQueryingDB] = useState(false);
 	const router = useRouter();
 	const matchesSmallDevice = useMediaQuery("(max-width: 600px)");
+
+	useEffect(() => {
+		const selectedTags = [];
+		for (const tag in discussionTags) {
+			if (discussionTags[tag] === true) {
+				selectedTags.push(tag.toLowerCase());
+			}
+		}
+		setQueryingDB(true);
+		getDiscussionsByTags(selectedTags)
+			.then((data) => {
+				setDiscussionsList(data);
+			})
+			.finally(() => {
+				setQueryingDB(false);
+			});
+	}, [discussionTags]);
 
 	function toggleFilterDrawer(open) {
 		setFilterDrawerIsOpen(open);
 	}
 
+	function onSelectValueChanged(event) {
+		setFilter(event.target.value);
+	}
+
+	function onCheckboxValueChanged(event) {
+		setDiscussionTags((snapshot) => {
+			snapshot[event.target.name] = event.target.checked;
+			return { ...snapshot };
+		});
+	}
+
 	const tagsElements = useMemo(() => {
 		return DISCUSSION_TAGS.map((tag) => (
 			<li key={tag}>
-				<Checkbox id={tag} label={tag} name={tag.toUpperCase()} />
+				<Checkbox
+					id={tag}
+					label={tag}
+					name={tag}
+					checked={discussionTags[tag]}
+					onChange={onCheckboxValueChanged}
+				/>
 			</li>
 		));
-	}, []);
+	}, [discussionTags]);
+
+	const discussions = useMemo(() => {
+		return discussionsList.map((discussion) => {
+			return (
+				<DiscussionItem
+					id={discussion.id}
+					title={discussion.title}
+					tag={discussion.tag}
+					creatorID={discussion.creator_id}
+				/>
+			);
+		});
+	}, [discussionsList]);
 
 	return (
 		<Fragment>
@@ -42,16 +101,28 @@ export default function Discussions() {
 					property="og:url"
 					content="https://animehaven.vercel.app/discussions"
 				/>
-				<meta name="description" content="COMING SOON" />
+				<meta
+					name="description"
+					content="Get information on the latest animes, compile and share lists of animes and have discussions about your favorite animes on Animehaven."
+				/>
 				<meta property="og:title" content="Animehaven | Discussions" />
-				<meta property="og:description" content="COMING SOON" />
+				<meta
+					property="og:description"
+					content="Get information on the latest animes, compile and share lists of animes and have discussions about your favorite animes on Animehaven."
+				/>
 				<meta name="twitter:title" content="Animehaven | Discussions" />
-				<meta name="twitter:description" content="COMING SOON" />
+				<meta
+					name="twitter:description"
+					content="Get information on the latest animes, compile and share lists of animes and have discussions about your favorite animes on Animehaven."
+				/>
 			</Head>
 			<PageContainer className="d-flex gap-2">
 				{!matchesSmallDevice && (
 					<div className="d-flex flex-column gap-3">
-						<Select title="Filter discussions">
+						<Select
+							title="Filter discussions"
+							value={filter}
+							onChange={onSelectValueChanged}>
 							<option value="all">All</option>
 							{profileID && (
 								<option value="your_discussions">Your Discussions</option>
@@ -110,6 +181,10 @@ export default function Discussions() {
 							minLength={4}
 							spellCheck={false}
 						/>
+						{queryingDB && <Loading />}
+						{!queryingDB && (
+							<ul style={{ marginTop: "10px" }}>{discussions}</ul>
+						)}
 					</div>
 				</div>
 			</PageContainer>
