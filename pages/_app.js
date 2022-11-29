9449171de5a0be445568e2e2f778329ea3094d81
Hermes <hermeschigoziri@gmail.com>
2022-11-29 19:51:02 +0100

    page should always have a title

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,22 @@ import Header from "../components/Header/Header";
 import { UserAuthContextProvider } from "../context/UserAuthContext";
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	const getLayout = Component.getLayout || ((page) => page);
 	return (
-		<UserAuthContextProvider>
-			<Header />
-			{getLayout(<Component {...pageProps} />)}
-			<Analytics />
-		</UserAuthContextProvider>
+		<Fragment>
+			<Head>
+				<title>Animehaven</title>
+			</Head>
+			<UserAuthContextProvider>
+				<Header />
+				{getLayout(<Component {...pageProps} />)}
+				<Analytics />
+			</UserAuthContextProvider>
+		</Fragment>
 	);
 }
 
