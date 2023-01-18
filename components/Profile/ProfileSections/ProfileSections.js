cbd1238f747e3bc1f8045e531a36355e9fea16ae
Hermes <hermeschigoziri@gmail.com>
2023-01-18 11:59:52 +0100

    use router to reload page

--- components/Profile/ProfileSections/ProfileSections.js
+++ components/Profile/ProfileSections/ProfileSections.js
@@ -415,7 +415,7 @@ export function EditProfile({ open, closeDialog }) {
 			await supabase.from("profiles").update(newData).eq("id", profileID);
 			closeDialog();
 			router.replace(`/users/${accountName}`);
-			window.location.reload();
+			router.reload();
 		} else {
 			triggerAlert("Update process failed. No user signed in", {
 				severity: "warning",
