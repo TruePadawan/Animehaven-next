ee2f067b96324541a517128edc505c69c07de5df
Hermes <hermeschigoziri@gmail.com>
2023-01-21 21:54:16 +0100

    minor ux improvement

--- components/Items/UserSearchResultItem/UserSearchResultItem.js
+++ components/Items/UserSearchResultItem/UserSearchResultItem.js
@@ -5,15 +5,17 @@ import styles from "./style.module.css";
 const UserSearchResultItem = ({ accountName, title, avatarURL, timestamp }) => {
 	const dateJoined = new Date(timestamp);
 	return (
-		<li className={styles.userItem}>
-			<Image src={avatarURL} alt={accountName} width={65} height={65} />
-			<div className="d-flex flex-column">
-				<Link href={`/users/${accountName}`}>{title}</Link>
-				<small
-					className={
-						styles.userJoinedOn
-					}>{`Joined ${dateJoined.toDateString()}`}</small>
-			</div>
+		<li>
+			<Link href={`/users/${accountName}`} className={styles.userItem}>
+				<Image src={avatarURL} alt={accountName} width={65} height={65} />
+				<div className="d-flex flex-column">
+					<span href={`/users/${accountName}`}>{title}</span>
+					<small
+						className={
+							styles.userJoinedOn
+						}>{`Joined ${dateJoined.toDateString()}`}</small>
+				</div>
+			</Link>
 		</li>
 	);
 };
