c0d7e8392fd80413d8718bae5eb873435ec44c27
Hermes <hermeschigoziri@gmail.com>
2023-01-21 21:56:37 +0100

    fix list item component not rendering a html list item

--- components/Items/AnimeSearchResultItem/AnimeSearchResultItem.js
+++ components/Items/AnimeSearchResultItem/AnimeSearchResultItem.js
@@ -3,12 +3,14 @@ import styles from "./styles.module.css";
 
 const AnimeSearchResultItem = (props) => {
 	return (
-		<Link href={props.linkTo} className={styles.item}>
-			<img src={props.photoURL} alt={props.title} />
-			<span className={styles["item-name"]} title={props.title}>
-				{props.title}
-			</span>
-		</Link>
+		<li>
+			<Link href={props.linkTo} className={styles.item}>
+				<img src={props.photoURL} alt={props.title} />
+				<span className={styles["item-name"]} title={props.title}>
+					{props.title}
+				</span>
+			</Link>
+		</li>
 	);
 };
 
