59d2332bf4a91a183e6376fdb1841e46789f15a4
hermes <hermeschigoziri@gmail.com>
2025-04-01 18:15:56 +0100

    code formatting

--- components/Authentication/Authentication.tsx
+++ components/Authentication/Authentication.tsx
@@ -1,70 +1,70 @@
 import {Alert, Button, ButtonGroup, Snackbar, SnackbarOrigin, styled} from "@mui/material";
-import { useContext, useState } from "react";
-import { UserAuthContext } from "../../context/UserAuthContext";
+import {useContext, useState} from "react";
+import {UserAuthContext} from "../../context/UserAuthContext";
 import UserAccountBtn from "./UserAccountBtn";
-import { Fragment } from "react";
-import { useRouter } from "next/router";
+import {Fragment} from "react";
+import {useRouter} from "next/router";
 
 const AuthBtn = styled(Button)(() => ({
-	backgroundColor: "darkslategray",
-	fontFamily: "inherit",
-	"&:hover": {
-		backgroundColor: "rgb(68, 115, 115)",
-	},
+    backgroundColor: "darkslategray",
+    fontFamily: "inherit",
+    "&:hover": {
+        backgroundColor: "rgb(68, 115, 115)",
+    },
 }));
 
 const Authentication = () => {
-	const { profileID } = useContext(UserAuthContext);
-	const [error, setError] = useState({ occurred: false, text: "" });
-	const router = useRouter();
+    const {profileID} = useContext(UserAuthContext);
+    const [error, setError] = useState({occurred: false, text: ""});
+    const router = useRouter();
 
-	const handleError = (text: string, error: HasErrorMessage) => {
-		setError({
-			occurred: true,
-			text: `${text} - ${error.message || error.error_description}`,
-		});
-	};
+    const handleError = (text: string, error: HasErrorMessage) => {
+        setError({
+            occurred: true,
+            text: `${text} - ${error.message || error.error_description}`,
+        });
+    };
 
-	const resetError = (event: React.SyntheticEvent<any> | Event, reason: string) => {
-		if (reason === "clickaway") {
-			return;
-		}
-		setError({ occurred: false, text: "" });
-	};
+    const resetError = (event: React.SyntheticEvent<any> | Event, reason: string) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setError({occurred: false, text: ""});
+    };
 
-	const userSignedIn = profileID !== null;
-	const alertAnchorOrigin: SnackbarOrigin = {
-		vertical: "top",
-		horizontal: "center",
-	};
-	return (
-		<>
-			{userSignedIn && (
-				<UserAccountBtn profileID={profileID} errorHandler={handleError} />
-			)}
-			{!userSignedIn && (
-				<Fragment>
-					<ButtonGroup>
-						<AuthBtn variant="contained" onClick={() => router.push("/signup")}>
-							Sign Up
-						</AuthBtn>
-						<AuthBtn variant="contained" onClick={() => router.push("/signin")}>
-							Sign In
-						</AuthBtn>
-					</ButtonGroup>
-					<Snackbar
-						open={error.occurred}
-						autoHideDuration={6000}
-						onClose={resetError}
-						anchorOrigin={alertAnchorOrigin}>
-						<Alert severity="error" sx={{ width: "100%" }}>
-							{error.text}
-						</Alert>
-					</Snackbar>
-				</Fragment>
-			)}
-		</>
-	);
+    const userSignedIn = profileID !== null;
+    const alertAnchorOrigin: SnackbarOrigin = {
+        vertical: "top",
+        horizontal: "center",
+    };
+    return (
+        <>
+            {userSignedIn && (
+                <UserAccountBtn profileID={profileID} errorHandler={handleError}/>
+            )}
+            {!userSignedIn && (
+                <Fragment>
+                    <ButtonGroup>
+                        <AuthBtn variant="contained" onClick={() => router.push("/signup")}>
+                            Sign Up
+                        </AuthBtn>
+                        <AuthBtn variant="contained" onClick={() => router.push("/signin")}>
+                            Sign In
+                        </AuthBtn>
+                    </ButtonGroup>
+                    <Snackbar
+                        open={error.occurred}
+                        autoHideDuration={6000}
+                        onClose={resetError}
+                        anchorOrigin={alertAnchorOrigin}>
+                        <Alert severity="error" sx={{width: "100%"}}>
+                            {error.text}
+                        </Alert>
+                    </Snackbar>
+                </Fragment>
+            )}
+        </>
+    );
 };
 
 export default Authentication;
--- components/Authentication/UserAccountBtn.tsx
+++ components/Authentication/UserAccountBtn.tsx
@@ -1,102 +1,102 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {
-	Menu,
-	MenuItem,
-	IconButton,
-	Avatar,
-	CircularProgress,
+    Menu,
+    MenuItem,
+    IconButton,
+    Avatar,
+    CircularProgress,
 } from "@mui/material";
 import Link from "next/link";
-import { DEFAULT_AVATAR_URL } from "../../utilities/app-utilities";
-import { getProfileData } from "../../utilities/app-utilities";
+import {DEFAULT_AVATAR_URL} from "../../utilities/app-utilities";
+import {getProfileData} from "../../utilities/app-utilities";
 import Image from "next/image";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import {useSupabaseClient} from "@supabase/auth-helpers-react";
 import {AccountMenuButtonProps} from "./types/UserAccountButton.types";
 import {Database} from "../../database.types";
 import {PostgrestError} from "@supabase/supabase-js";
 
 const UserAccountBtn = (props: AccountMenuButtonProps) => {
-	const supabase = useSupabaseClient<Database>();
-	const [accountName, setAccountName] = useState("");
-	const [menuAnchorEl, setMenuAnchorEl] = useState<Element|null>(null);
-	const [photoSrc, setPhotoSrc] = useState(DEFAULT_AVATAR_URL);
-	const [loading, setLoading] = useState(true);
-	const isMenuOpen = Boolean(menuAnchorEl);
-	const { profileID, errorHandler } = props;
+    const supabase = useSupabaseClient<Database>();
+    const [accountName, setAccountName] = useState("");
+    const [menuAnchorEl, setMenuAnchorEl] = useState<Element | null>(null);
+    const [photoSrc, setPhotoSrc] = useState(DEFAULT_AVATAR_URL);
+    const [loading, setLoading] = useState(true);
+    const isMenuOpen = Boolean(menuAnchorEl);
+    const {profileID, errorHandler} = props;
 
-	useEffect(() => {
-		getProfileData(supabase, "*", profileID).then(({ account_name, avatar_url }) => {
-			setAccountName(account_name);
-			setPhotoSrc(avatar_url);
-			setLoading(false);
-		});
-	}, [profileID, supabase]);
+    useEffect(() => {
+        getProfileData(supabase, "*", profileID).then(({account_name, avatar_url}) => {
+            setAccountName(account_name);
+            setPhotoSrc(avatar_url);
+            setLoading(false);
+        });
+    }, [profileID, supabase]);
 
-	const openMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
-		setMenuAnchorEl(e.currentTarget as Element);
-	};
+    const openMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setMenuAnchorEl(e.currentTarget as Element);
+    };
 
-	const closeMenu = () => {
-		setMenuAnchorEl(null);
-	};
+    const closeMenu = () => {
+        setMenuAnchorEl(null);
+    };
 
-	const signUserOut = () => {
-		setLoading(true);
-		supabase.auth
-			.signOut()
-			.then(() => {
-				setLoading(false);
-				window.location.reload();
-			})
-			.catch((error) => {
-				errorHandler("Failed to sign out!", error as PostgrestError);
-			});
-	};
+    const signUserOut = () => {
+        setLoading(true);
+        supabase.auth
+            .signOut()
+            .then(() => {
+                setLoading(false);
+                window.location.reload();
+            })
+            .catch((error) => {
+                errorHandler("Failed to sign out!", error as PostgrestError);
+            });
+    };
 
-	return (
-		<Fragment>
-			{!loading && (
-				<Fragment>
-					<IconButton
-						onClick={openMenu}
-						aria-controls={isMenuOpen ? "account-menu" : undefined}
-						aria-haspopup="true"
-						aria-expanded={isMenuOpen ? "true" : undefined}>
-						<Avatar alt={accountName} sx={{ color: "black" }}>
-							<Image
-								style={{ objectFit: "contain" }}
-								src={photoSrc}
-								alt={accountName}
-								width={40}
-								height={40}
-								quality={100}
-								title="Account"
-							/>
-						</Avatar>
-					</IconButton>
-					<Menu
-						id="account-menu"
-						anchorEl={menuAnchorEl}
-						open={isMenuOpen}
-						onClick={closeMenu}
-						PaperProps={{
-							sx: {
-								backgroundColor: "#121212",
-								color: "white",
-							},
-						}}>
-						<MenuItem className="font-rubik">
-							<Link href={`/users/${accountName}`}>My Profile</Link>
-						</MenuItem>
-						<MenuItem className="font-rubik" onClick={signUserOut}>
-							Sign Out
-						</MenuItem>
-					</Menu>
-				</Fragment>
-			)}
-			{loading && <CircularProgress sx={{ color: "goldenrod" }} />}
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            {!loading && (
+                <Fragment>
+                    <IconButton
+                        onClick={openMenu}
+                        aria-controls={isMenuOpen ? "account-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={isMenuOpen ? "true" : undefined}>
+                        <Avatar alt={accountName} sx={{color: "black"}}>
+                            <Image
+                                style={{objectFit: "contain"}}
+                                src={photoSrc}
+                                alt={accountName}
+                                width={40}
+                                height={40}
+                                quality={100}
+                                title="Account"
+                            />
+                        </Avatar>
+                    </IconButton>
+                    <Menu
+                        id="account-menu"
+                        anchorEl={menuAnchorEl}
+                        open={isMenuOpen}
+                        onClick={closeMenu}
+                        PaperProps={{
+                            sx: {
+                                backgroundColor: "#121212",
+                                color: "white",
+                            },
+                        }}>
+                        <MenuItem className="font-rubik">
+                            <Link href={`/users/${accountName}`}>My Profile</Link>
+                        </MenuItem>
+                        <MenuItem className="font-rubik" onClick={signUserOut}>
+                            Sign Out
+                        </MenuItem>
+                    </Menu>
+                </Fragment>
+            )}
+            {loading && <CircularProgress sx={{color: "goldenrod"}}/>}
+        </Fragment>
+    );
 };
 
 export default UserAccountBtn;
