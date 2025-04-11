e499812b9e439b0d1527d68caa48d308cc4e8cae
hermes <hermeschigoziri@gmail.com>
2025-04-11 22:58:41 +0100

    convert SearchBar, Select and ShareButton component to TypeScript

---
+++ components/SearchBar/SearchBar.tsx
@@ -0,0 +1,79 @@
+import {memo, useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {v4 as uuid} from "uuid";
+import SearchInput from "../Input/SearchInput/SearchInput";
+import styles from "./SearchBar.module.css";
+import {useMediaQuery} from "@mui/material";
+import Select from "../Select/Select";
+
+interface SearchBarProps {
+    searchText: string | null;
+    searchCategory: string | null;
+    searchCategories: string[];
+}
+
+const SearchBar = ({searchCategories, searchText, searchCategory}: SearchBarProps) => {
+    const router = useRouter();
+    const [inputVal, setInputVal] = useState(searchText || "");
+    const [selectVal, setSelectVal] = useState(searchCategories[0]);
+    const matchesSmallDevice = useMediaQuery("(max-width: 500px)");
+
+    const updatePageURL = (searchText: string, searchCategory: string) => {
+        router.push(`/search?cat=${searchCategory}&text=${searchText}`);
+    };
+
+    useEffect(() => {
+        setInputVal(searchText || "");
+        setSelectVal(() => {
+            if (searchCategory) return searchCategory.toUpperCase();
+            return searchCategories[0].toUpperCase();
+        });
+    }, [searchCategory, searchCategories, searchText]);
+
+    const searchHandler = () => {
+        updatePageURL(
+            inputVal.toLowerCase(),
+            selectVal.toLowerCase()
+        );
+    };
+
+    const componentClassName = matchesSmallDevice
+        ? "d-flex flex-column w-100 gap-2"
+        : styles.searchBar;
+
+    const selectOptionsEl = searchCategories.map((option) => (
+        <option key={uuid()} value={option.toUpperCase()}>
+            {option}
+        </option>
+    ));
+    return (
+        <div className={componentClassName}>
+            {matchesSmallDevice && (
+                <Select
+                    className="align-self-start"
+                    title="Search Category"
+                    value={selectVal}
+                    onChange={(e) => setSelectVal(e.target.value)}>
+                    {selectOptionsEl}
+                </Select>
+            )}
+            {!matchesSmallDevice && (
+                <select
+                    className={styles.searchCategory}
+                    title="Search Category"
+                    value={selectVal}
+                    onChange={(e) => setSelectVal(e.target.value)}>
+                    {selectOptionsEl}
+                </select>
+            )}
+            <SearchInput
+                searchFunc={searchHandler}
+                value={inputVal}
+                onChange={(e) => setInputVal(e.target.value)}
+                minLength={3}
+            />
+        </div>
+    );
+};
+
+export default memo(SearchBar);
--- components/Select/Select.js
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.css";
-
-export default function Select({children, className, compRef, ...selectAttr}) {
-  return (
-    <select className={`${styles.select} ${className || ""}`} {...selectAttr} ref={compRef}>
-        {children}
-    </select>
-  );
-}
