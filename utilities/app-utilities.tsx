7333fe7d763457e16b284999c23874a7811fcbbf
hermes <hermeschigoziri@gmail.com>
2025-04-13 08:39:59 +0100

    port utility functions to TypeScript

--- utilities/app-utilities.tsx
+++ utilities/app-utilities.tsx
@@ -1,7 +1,7 @@
 import {filetypemime} from "magic-bytes.js";
 import {PROFILE_IMG_MAX_SIZE} from "./global-constants";
 import {SupabaseClient} from "@supabase/supabase-js";
-import {Database, TablesInsert} from "../database.types";
+import {Database, Tables, TablesInsert} from "../database.types";
 import {Anime} from "@tutkli/jikan-ts";
 
 
@@ -27,7 +27,7 @@ export function createProfile(supabase: SupabaseClient<Database>, accountData: T
 	return supabase.from("profiles").insert(accountData);
 }
 
-export async function hasProfile(supabase: SupabaseClient<Database>, profileId: string) {
+export async function hasProfile(supabase: SupabaseClient<Database>, profileId: Tables<"profiles">["id"]) {
 	const { count } = await supabase
 		.from("profiles")
 		.select("*", { count: "exact", head: true })
@@ -38,7 +38,7 @@ export async function hasProfile(supabase: SupabaseClient<Database>, profileId: 
 
 export async function getCommentsData(
 	supabase: SupabaseClient<Database>,
-	instanceId: string,
+	instanceId: Tables<"comments">["instance_id"],
 	limit: number,
 	startAfterIndex?: number,
 ) {
@@ -64,65 +64,66 @@ export async function getCommentsData(
 	}
 }
 
-export async function getCommentData(supabase, commentID, fields = "*") {
+export async function getCommentData(supabase: SupabaseClient<Database>, commentId: Tables<"comments">["id"], fields = "*") {
 	const { data } = await supabase
 		.from("comments")
 		.select(fields)
-		.eq("id", commentID)
+		.eq("id", commentId)
 		.throwOnError()
 		.limit(1)
 		.single();
 	return data;
 }
 
-export async function getProfileData(supabase, fields, profileID) {
+export async function getProfileData(supabase: SupabaseClient<Database>, fields: string, profileId: Tables<"profiles">["id"]) {
 	const { data } = await supabase
 		.from("profiles")
 		.select(fields)
-		.eq("id", profileID)
+		.eq("id", profileId)
 		.throwOnError()
 		.limit(1)
 		.single();
 	return data;
 }
 
-export async function getProfileID(supabase, accountName) {
-	const { data } = await supabase
+export async function getProfileID(supabase: SupabaseClient<Database>, accountName: string) {
+	const { data, error } = await supabase
 		.from("profiles")
 		.select("id")
-		.eq("account_name", accountName)
-		.throwOnError();
+		.eq("account_name", accountName);
+	if (error) throw error;
 	if (data.length === 1) {
 		return data[0].id;
 	}
 	return null;
 }
 
-export async function getReviewUpvoteList(supabase, reviewID) {
-	const { data } = await supabase
+export async function getReviewUpvoteList(supabase: SupabaseClient<Database>, reviewId: Tables<"item_reviews">["id"]) {
+	const { data, error } = await supabase
 		.from("item_reviews")
 		.select("upvoted_by")
-		.eq("id", reviewID)
+		.eq("id", reviewId)
 		.limit(1)
 		.single();
+	if (error) throw error;
 	return data.upvoted_by;
 }
 
-export async function reviewExist(supabase, reviewID) {
+export async function reviewExist(supabase: SupabaseClient<Database>, reviewId: Tables<"item_reviews">["id"]) {
 	const { count } = await supabase
 		.from("item_reviews")
 		.select("*", { count: "exact", head: true })
-		.eq("id", reviewID);
+		.eq("id", reviewId);
 	return count === 1;
 }
 
-export async function toggleUpvoteForReview(supabase, reviewID, profileID) {
-	if (!profileID) {
+export async function toggleUpvoteForReview(supabase: SupabaseClient<Database>, reviewId: Tables<"item_reviews">["id"], profileId: Tables<"profiles">["id"]) {
+	if (!profileId) {
 		throw new Error("NO_PROFILE_ID_SPECIFIED");
 	}
 	// CONFIRM THAT REVIEW EXISTS
 	try {
-		const exist = await reviewExist(supabase, reviewID);
+		const exist = await reviewExist(supabase, reviewId);
 		if (!exist) {
 			return {
 				status: "FAILED",
@@ -134,8 +135,8 @@ export async function toggleUpvoteForReview(supabase, reviewID, profileID) {
 	}
 
 	const { data, error } = await supabase.rpc("toggle_review_upvote", {
-		profile_id: profileID,
-		review_id: reviewID,
+		profile_id: profileId,
+		review_id: reviewId,
 	});
 
 	if (error) throw error;
@@ -145,13 +146,13 @@ export async function toggleUpvoteForReview(supabase, reviewID, profileID) {
 	};
 }
 
-export async function deleteReview(supabase, reviewID, profileID) {
-	if (!profileID) {
+export async function deleteReview(supabase: SupabaseClient<Database>, reviewId: Tables<"item_reviews">["id"], profileId: Tables<"profiles">["id"]) {
+	if (!profileId) {
 		throw new Error("NO_PROFILE_ID_SPECIFIED");
 	}
 	// CONFIRM THAT REVIEW EXISTS
 	try {
-		const exist = await reviewExist(supabase, reviewID);
+		const exist = await reviewExist(supabase, reviewId);
 		if (!exist) {
 			return {
 				status: "FAILED",
@@ -165,23 +166,24 @@ export async function deleteReview(supabase, reviewID, profileID) {
 	await supabase
 		.from("item_reviews")
 		.delete()
-		.eq("id", reviewID)
-		.eq("creator_id", profileID)
+		.eq("id", reviewId)
+		.eq("creator_id", profileId)
 		.throwOnError();
+
 	return {
 		status: "COMPLETE",
 		code: "REVIEW_DELETED",
 	};
 }
 
-export async function toggleUpvoteForComment(supabase, commentID, profileID) {
-	if (!profileID) {
+export async function toggleUpvoteForComment(supabase: SupabaseClient<Database>, commentId: Tables<"comments">["id"], profileId: Tables<"profiles">["id"]) {
+	if (!profileId) {
 		throw new Error("NO_PROFILE_ID_SPECIFIED");
 	}
 
 	const { data, error } = await supabase.rpc("toggle_comment_upvote", {
-		comment_id: commentID,
-		profile_id: profileID,
+		comment_id: commentId,
+		profile_id: profileId,
 	});
 
 	if (error) throw error;
@@ -191,29 +193,24 @@ export async function toggleUpvoteForComment(supabase, commentID, profileID) {
 	};
 }
 
-export async function getUserItemRecommendations(supabase, profileID) {
-	const response = await supabase
+export async function getUserItemRecommendations(supabase: SupabaseClient<Database>, profileId: Tables<"profiles">["id"]) {
+	return supabase
 		.from("item_recommendations")
 		.select()
-		.eq("recommended_by", profileID)
+		.eq("recommended_by", profileId)
 		.throwOnError();
-	return response;
 }
 
-export async function getUserItemReviews(supabase, profileID) {
-	const response = await supabase
+export async function getUserItemReviews(supabase: SupabaseClient<Database>, profileId: Tables<"profiles">["id"]) {
+	return supabase
 		.from("item_reviews")
 		.select()
-		.eq("creator_id", profileID)
+		.eq("creator_id", profileId)
 		.throwOnError();
-	return response;
 }
 
-export async function getDiscussionsByTags(supabase, tags, profileID = null) {
-	if (!(tags instanceof Array)) {
-		throw new Error("Invalid params - tags must be an array");
-	}
-	if (profileID === null) {
+export async function getDiscussionsByTags(supabase: SupabaseClient<Database>, tags: string[], profileId: Tables<"profiles">["id"] | null = null) {
+	if (profileId === null) {
 		const { data } = await supabase
 			.from("discussions")
 			.select()
@@ -225,49 +222,50 @@ export async function getDiscussionsByTags(supabase, tags, profileID = null) {
 			.from("discussions")
 			.select()
 			.in("tag", tags)
-			.eq("creator_id", profileID)
+			.eq("creator_id", profileId)
 			.throwOnError();
 		return data;
 	}
 }
 
-export async function getDiscussionByID(supabase, discussionID) {
-	if (!discussionID) {
-		throw new Error(`Invalid argument passed - ${discussionID}`);
+export async function getDiscussionByID(supabase: SupabaseClient<Database>, discussionId: Tables<"discussions">["id"]) {
+	if (!discussionId) {
+		throw new Error(`Invalid argument passed - ${discussionId}`);
 	}
-	const { data } = await supabase
+	const { data, error } = await supabase
 		.from("discussions")
 		.select("*")
-		.eq("id", discussionID)
-		.throwOnError();
+		.eq("id", discussionId);
+	if (error) throw error;
+	
 	if (data.length === 0) {
-		throw new Error(`Discussion with id - ${discussionID} not found!`);
+		throw new Error(`Discussion with id - ${discussionId} not found!`);
 	}
 	return data[0];
 }
 
-export async function getDiscussionByAccountName(supabase, accountName) {
+export async function getDiscussionByAccountName(supabase: SupabaseClient<Database>, accountName: string) {
 	if (!accountName) {
 		throw new Error(`Invalid argument passed - ${accountName}`);
 	}
 	const id = await getProfileID(supabase, accountName);
-	const { data } = await supabase
+	const { data, error } = await supabase
 		.from("discussions")
 		.select("*")
-		.eq("creator_id", id)
-		.throwOnError();
+		.eq("creator_id", id);
+	if (error) throw error;
 	return data;
 }
 
-export async function getListByID(supabase, listID) {
-	if (!listID) {
-		throw new Error(`Invalid argument passed - ${listID}`);
+export async function getListByID(supabase: SupabaseClient<Database>, animelistId: Tables<"anime_lists">["id"]) {
+	if (!animelistId) {
+		throw new Error(`Invalid argument passed - ${animelistId}`);
 	}
-	const { data } = await supabase
+	const { data, error } = await supabase
 		.from("anime_lists")
 		.select("*")
-		.eq("id", listID)
-		.throwOnError();
+		.eq("id", animelistId);
+	if (error) throw error;
 	if (data.length === 0) {
 		throw new Error(
 			"List data could not be retrieved. It might be private or deleted!"
@@ -276,7 +274,7 @@ export async function getListByID(supabase, listID) {
 	return data[0];
 }
 
-export function numberToString(count, appendString) {
+export function numberToString(count: number, appendString: string) {
 	if (count === 0) {
 		return `0 ${appendString}s`;
 	} else if (count === 1) {
@@ -292,101 +290,105 @@ export function numberToString(count, appendString) {
 	}
 }
 
-export async function getReviewByUser(supabase, animeID, profileID) {
-	const { data } = await supabase
+export async function getReviewByUser(supabase: SupabaseClient<Database>, animeId: string, profileId: string) {
+	const { data, error } = await supabase
 		.from("item_reviews")
 		.select()
-		.eq("item_id", animeID)
-		.eq("creator_id", profileID)
-		.throwOnError();
+		.eq("item_id", animeId)
+		.eq("creator_id", profileId);
+	if (error) throw error;
 	return data;
 }
 
 export async function getReviewsData(
-	supabase,
-	animeID,
-	limit,
-	startAfterIndex,
-	profileID
+	supabase: SupabaseClient<Database>,
+	animeId: string,
+	limit: number,
+	startAfterIndex?: number,
+	profileId?: string
 ) {
-	let list = [];
+	let list: Tables<"item_reviews">[] = [];
 	let totalReviewsCount = 0;
-	const response = { data: [], count: 0 };
 	// GET USER REVIEW FIRST BEFORE OTHER REVIEWS IF PROFILE ID SPECIFIED
-	if (profileID !== undefined) {
-		const userReview = await getReviewByUser(supabase, animeID, profileID);
+	if (profileId !== undefined) {
+		const userReview = await getReviewByUser(supabase, animeId, profileId);
 		if (userReview.length > 0) {
 			list.push(userReview[0]);
 			totalReviewsCount += 1;
 		}
 
 		if (startAfterIndex !== undefined) {
-			const { data, count } = await supabase
+			const { data, error, count } = await supabase
 				.from("item_reviews")
 				.select("*", { count: "exact" })
-				.eq("item_id", animeID)
-				.neq("creator_id", profileID)
+				.eq("item_id", animeId)
+				.neq("creator_id", profileId)
 				.limit(limit)
 				.lt("index", startAfterIndex)
-				.order("created_at", { ascending: false })
-				.throwOnError();
-			totalReviewsCount += count;
+				.order("created_at", { ascending: false });
+			if (error) throw error;
+
+			totalReviewsCount += count ?? 0;
 			list = list.concat(data);
 		} else {
-			const { data, count } = await supabase
+			const { data, error, count } = await supabase
 				.from("item_reviews")
 				.select("*", { count: "exact" })
-				.eq("item_id", animeID)
-				.neq("creator_id", profileID)
+				.eq("item_id", animeId)
+				.neq("creator_id", profileId)
 				.limit(limit)
-				.order("created_at", { ascending: false })
-				.throwOnError();
-			totalReviewsCount += count;
+				.order("created_at", { ascending: false });
+			if (error) throw error;
+
+			totalReviewsCount += count ?? 0;
 			list = list.concat(data);
 		}
 	} else {
 		if (startAfterIndex !== null) {
-			const { data, count } = await supabase
+			const { data, error, count } = await supabase
 				.from("item_reviews")
 				.select("*", { count: "exact" })
-				.eq("item_id", animeID)
+				.eq("item_id", animeId)
 				.limit(limit)
 				.lt("index", startAfterIndex)
-				.order("created_at", { ascending: false })
-				.throwOnError();
-			totalReviewsCount += count;
+				.order("created_at", { ascending: false });
+			if (error) throw error;
+
+			totalReviewsCount += count ?? 0;
 			list = list.concat(data);
 		} else {
-			const { data, count } = await supabase
+			const { data, error, count } = await supabase
 				.from("item_reviews")
 				.select("*", { count: "exact" })
-				.eq("item_id", animeID)
+				.eq("item_id", animeId)
 				.limit(limit)
-				.order("created_at", { ascending: false })
-				.throwOnError();
-			totalReviewsCount += count;
+				.order("created_at", { ascending: false });
+			if (error) throw error;
+
+			totalReviewsCount += count ?? 0;
 			list = list.concat(data);
 		}
 	}
-	response.data = list;
-	response.count = totalReviewsCount;
-	return response;
+
+	return { data: list, count: totalReviewsCount };
 }
 
-export async function getRecentItems(supabase, type, profileID) {
-	const { data } = await supabase
+export async function getRecentItems(supabase: SupabaseClient<Database>, type: string, profileId: string) {
+	const { data, error } = await supabase
 		.from("recent_items")
 		.select(type)
-		.eq("profile_id", profileID)
-		.throwOnError();
+		.eq("profile_id", profileId);
+	if (error) throw error;
+	// @ts-ignore
+	// TODO: split recent_items table into separate tables
 	return data[0][type];
 }
 
-export async function setRecentItem(supabase, type, profileID, item) {
-	const recentItems = await getRecentItems(supabase, type, profileID);
+export async function setRecentItem(supabase: SupabaseClient<Database>, type: string, profileId: string, item: any) {
+	const recentItems = await getRecentItems(supabase, type, profileId);
 	let itemAlreadyExists = false;
 	if (type === "animes") {
-		itemAlreadyExists = recentItems.some((r_item) => r_item.id === item.id);
+		itemAlreadyExists = recentItems.some((r_item: { id: any; }) => r_item.id === item.id);
 	} else {
 		itemAlreadyExists = recentItems.includes(item);
 	}
@@ -401,24 +403,28 @@ export async function setRecentItem(supabase, type, profileID, item) {
 				.update({
 					[type]: updatedRecentItems,
 				})
-				.eq("profile_id", profileID)
+				.eq("profile_id", profileId)
 				.throwOnError();
 		} catch (error) {
-			throw new Error(`Failed to update recent items - ${type}`);
+			throw new Error(`Failed to update recent items - ${getErrorMessage(error)}`);
 		}
 	}
 }
 
-export function verifyProfileImage(file, onVerificationComplete) {
+// TODO: look for an alternative library to detect file types accurately
+export function verifyProfileImage(file: File, onVerificationComplete: (isValid: boolean) => void) {
 	let isValidSize = file.size <= PROFILE_IMG_MAX_SIZE;
 	if (isValidSize) {
 		// CONFIRM FILE IS AN IMAGE
 		const fileReader = new FileReader();
 		fileReader.addEventListener("loadend", (event) => {
+			// @ts-ignore
 			const bytes = new Uint8Array(event.target.result);
+			// @ts-ignore
 			const fileType = filetypemime(bytes);
 			let isValid = false;
 			if (fileType.length > 0) {
+				// @ts-ignore
 				isValid = fileType.at(0).split("/").at(0) === "image";
 			}
 			onVerificationComplete(isValid);
@@ -429,6 +435,6 @@ export function verifyProfileImage(file, onVerificationComplete) {
 	}
 }
 
-export function getErrorMessage(error) {
+export function getErrorMessage(error: any) {
 	return error?.message || error?.error_description || "";
 }
