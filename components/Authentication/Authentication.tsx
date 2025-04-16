d74eba88d13ff2a6d789a421a1323fec47ddf383
hermes <hermeschigoziri@gmail.com>
2025-04-16 19:12:27 +0100

    refactor: remove email authentication

--- components/Authentication/Authentication.tsx
+++ components/Authentication/Authentication.tsx
@@ -1,70 +1,54 @@
-import {Alert, Button, ButtonGroup, Snackbar, SnackbarOrigin, styled} from "@mui/material";
-import {useContext, useState} from "react";
-import {UserAuthContext} from "../../context/UserAuthContext";
+import { Alert, Snackbar, SnackbarOrigin } from "@mui/material";
+import { Fragment, useContext, useState } from "react";
+import { UserAuthContext } from "../../context/UserAuthContext";
 import ProfileMenuButton from "./ProfileMenuButton";
-import {Fragment} from "react";
-import {useRouter} from "next/router";
-import {HasErrorMessage, ResetAlert} from "../../utilities/global.types";
-
-const AuthBtn = styled(Button)(() => ({
-    backgroundColor: "darkslategray",
-    fontFamily: "inherit",
-    "&:hover": {
-        backgroundColor: "rgb(68, 115, 115)",
-    },
-}));
+import { HasErrorMessage, ResetAlert } from "../../utilities/global.types";
+import OAuthButton from "./OAuthButton";
 
 const Authentication = () => {
-    const {profileID} = useContext(UserAuthContext);
-    const [error, setError] = useState({occurred: false, text: ""});
-    const router = useRouter();
+  const { profileID } = useContext(UserAuthContext);
+  const [error, setError] = useState({ occurred: false, text: "" });
 
-    const handleError = (text: string, error: HasErrorMessage) => {
-        setError({
-            occurred: true,
-            text: `${text} - ${error.message || error.error_description}`,
-        });
-    };
+  const handleError = (text: string, error: HasErrorMessage) => {
+    setError({
+      occurred: true,
+      text: `${text} - ${error.message || error.error_description}`,
+    });
+  };
 
-    const resetError: ResetAlert = (event, reason) => {
-        if (reason !== "clickaway") {
-            setError({occurred: false, text: ""});
-        }
-    };
+  const resetError: ResetAlert = (event, reason) => {
+    if (reason !== "clickaway") {
+      setError({ occurred: false, text: "" });
+    }
+  };
 
-    const userSignedIn = profileID !== undefined;
-    const alertAnchorOrigin: SnackbarOrigin = {
-        vertical: "top",
-        horizontal: "center",
-    };
-    return (
-        <>
-            {userSignedIn && (
-                <ProfileMenuButton profileID={profileID} errorHandler={handleError}/>
-            )}
-            {!userSignedIn && (
-                <Fragment>
-                    <ButtonGroup>
-                        <AuthBtn variant="contained" onClick={() => router.push("/signup")}>
-                            Sign Up
-                        </AuthBtn>
-                        <AuthBtn variant="contained" onClick={() => router.push("/signin")}>
-                            Sign In
-                        </AuthBtn>
-                    </ButtonGroup>
-                    <Snackbar
-                        open={error.occurred}
-                        autoHideDuration={6000}
-                        onClose={resetError}
-                        anchorOrigin={alertAnchorOrigin}>
-                        <Alert severity="error" sx={{width: "100%"}}>
-                            {error.text}
-                        </Alert>
-                    </Snackbar>
-                </Fragment>
-            )}
-        </>
-    );
+  const userSignedIn = profileID !== undefined;
+  const alertAnchorOrigin: SnackbarOrigin = {
+    vertical: "top",
+    horizontal: "center",
+  };
+  return (
+    <>
+      {userSignedIn && (
+        <ProfileMenuButton profileID={profileID} errorHandler={handleError} />
+      )}
+      {!userSignedIn && (
+        <Fragment>
+          <OAuthButton />
+          <Snackbar
+            open={error.occurred}
+            autoHideDuration={6000}
+            onClose={resetError}
+            anchorOrigin={alertAnchorOrigin}
+          >
+            <Alert severity="error" sx={{ width: "100%" }}>
+              {error.text}
+            </Alert>
+          </Snackbar>
+        </Fragment>
+      )}
+    </>
+  );
 };
 
 export default Authentication;
