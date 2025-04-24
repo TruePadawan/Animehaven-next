2447a8a6a04b83a80d09e211575527ff14cef35d
hermes <hermeschigoziri@gmail.com>
2025-04-24 09:46:06 +0100

    refactor: update database migration to set a default empty array for profile.saved_lists

--- supabase/migrations/20250424080652_schema_change_saved_lists_to_array_bigint.sql
+++ supabase/migrations/20250424080652_schema_change_saved_lists_to_array_bigint.sql
@@ -1,2 +1,3 @@
 alter table "public"."profiles" alter column "saved_lists" drop default;
 alter table "public"."profiles" alter column "saved_lists" type bigint[] using array[]::bigint[];
+alter table "public"."profiles" alter column "saved_lists" set default '{}'::bigint[];
